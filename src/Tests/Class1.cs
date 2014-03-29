65ca46e1dc28eb1eb72f5baf88b60f98f5444a70
Ronnie Overby <roverby@coretechs.net>
2014-03-29 18:18:24 -0400

    intial commit

---
+++ src/Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniqueNamespace;
+
+namespace Tests
+{
+    public class Class1
+    {
+        public void Test1()
+        {
+            var sql = new SqlBuilder();
+        }
+
+    }
+}
