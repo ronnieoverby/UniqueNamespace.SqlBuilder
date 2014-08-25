479a5cbf6bdac504e95b0b208c2200f7af77b0a1
Ronnie Overby <roverby@coretechs.net>
2014-08-25 11:42:52 -0400

    added SqlPredicate

---
+++ src/SqlBuilder/SqlPredicate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace UniqueNamespace
+{
+    public class SqlPredicate
+    {
+        public string Sql { get; private set; }
+
+        public SqlPredicate(string sql)
+        {
+            if (sql == null) throw new ArgumentNullException("sql");
+
+            if (sql.IsNullOrWhiteSpace())
+                throw new ArgumentOutOfRangeException("sql", "sql was empty");
+
+            Sql = sql;
+        }
+
+        public SqlPredicate And(SqlPredicate p)
+        {
+            return string.Format("( {0} ) AND ( {1} )", Sql, p.Sql);
+        }
+
+        public SqlPredicate Or(SqlPredicate p)
+        {
+            return string.Format("( {0} ) OR ( {1} )", Sql, p.Sql);
+        }
+
+        public static implicit operator SqlPredicate(string sql)
+        {
+            return new SqlPredicate(sql);
+        }
+
+        public override string ToString()
+        {
+            return Sql;
+        }
+    }
+}
---
+++ src/Tests/PredicateTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using UniqueNamespace;
+
+namespace Tests
+{
+    public class PredicateTests
+    {
+        [Test]
+        public void CanAndSomeStuff()
+        {
+            SqlPredicate p1 = "1 = 1";
+            SqlPredicate p2 = "2 = 2";
+
+            var final = p1.And(p2);
+
+            final.Sql.Should().Be("( 1 = 1 ) AND ( 2 = 2 )");
+        }
+        
+        [Test]
+        public void CanOrSomeStuff()
+        {
+            SqlPredicate p1 = "1 = 1";
+            SqlPredicate p2 = "2 = 2";
+
+            var final = p1.Or(p2);
+
+            final.Sql.Should().Be("( 1 = 1 ) OR ( 2 = 2 )");
+        }
+        
+        [Test]
+        public void CanComposeSomeStuff()
+        {
+            SqlPredicate p1 = "1 = 1";
+            SqlPredicate p2 = "2 = 2";
+            SqlPredicate p3 = "10 = 20";
+            SqlPredicate p4 = "something IS NULL";
+
+            var p5 = p1.And(p2);
+            var p6 = p3.And(p4);
+            var final = p5.Or(p6);
+
+            final.Sql.Should().Be("( ( 1 = 1 ) AND ( 2 = 2 ) ) OR ( ( 10 = 20 ) AND ( something IS NULL ) )");
+        }
+    }
+}
